3951f053f1b6c82c0c7bca21dfed4389305e1577
rodmahe <rodmahe@gmail.com>
2017-06-08 11:17:42 +0200

    charge plan map

--- src/contact.php
+++ src/contact.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+   
+    
+
+
+
+
 <?php 
 include('requete.php');
 include('function.php');
@@ -5,3 +13,42 @@ include('head.php');
 include('main.php');
 include('footer.php');
 ?>
+
+<body>
+  
+  
+  <div>
+      
+      
+      
+      
+      
+      
+  </div>
+   
+   
+   <div class="Plan">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5458.620245341075!2d0.5351851433735059!3d46.837584641479765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fd076efc1d8e09%3A0x4edc8e974b559244!2sRond-Point+de+Pila%2C+86100+Ch%C3%A2tellerault!5e0!3m2!1sfr!2sfr!4v1496910460330" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+       
+       
+       
+       
+       
+   </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</body>
+
+</html>
