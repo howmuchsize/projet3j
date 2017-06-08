f9a4502a117ed4f9780a3d208b25619e9c21907d
IsenbrandtBaptiste <baptiste.isenbrandt@gmail.com>
2017-06-08 12:01:18 +0200

    auth

--- src/requete.php
+++ src/requete.php
@@ -7,5 +7,15 @@ tant que marquer le include('requete.php')
 */
 
 // Connection a la base de donnée 
+try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
+
+// Preparation des requetes de base de la base de donnée 
 
 ?>
